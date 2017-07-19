da4cd78df11a24ea6cff6233f0226875b5ca0df1
killyb <mike.kilberry@gmail.com>
2017-07-18 23:57:32 -0700

    ami fix

--- aws-vars.tf
+++ aws-vars.tf
@@ -28,15 +28,16 @@ variable "key_name" {
 
 variable "aws_region" {
   description = "AWS region to launch servers."
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
 # Ubuntu Precise 12.04 LTS (x64)
 variable "aws_amis" {
   default = {
     eu-west-1 = "ami-b1cf19c6"
-    us-east-1 = "ami-de7ab6b6"
+    us-east-1 = "ami-d15a75c7"
     us-west-1 = "ami-3f75767a"
     us-west-2 = "ami-21f78e11"
   }
 }
+
