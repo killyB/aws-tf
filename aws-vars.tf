626711c2db4441fb5a4f5d5374afe8d01ad68a7f
killyb <mike.kilberry@gmail.com>
2017-07-18 23:24:32 -0700

    1st commit

---
+++ aws-vars.tf
@@ -0,0 +1,13 @@
+//AWS client key to provision to AWS
+variable "access_key" {
+    type = "string"
+}
+//AWS client secret to provision to AWS
+variable "secret_key" {
+    type = "string"
+}
+
+//AWS region to deploy to
+variable "region" {
+    type = "string"
+}
